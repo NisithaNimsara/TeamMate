8e99a0089992ba69e9034d98a8b11567d2160354
NisithaNimsara <kvsnsd@gmail.com>
2025-11-13 00:15:55 +0530

    Team class build

---
+++ src/Team.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Team {
+
+    // attribute
+    private final List<Participant> members = new ArrayList<>();
+
+    // getter
+    public List<Participant> members() {
+        return members;
+    }
+
+}
