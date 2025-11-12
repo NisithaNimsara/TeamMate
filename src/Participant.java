be750bdcfcd9185f60153ee662263f72dfdf33e5
NisithaNimsara <kvsnsd@gmail.com>
2025-11-13 00:02:59 +0530

    Participant record class creation (without personality type)

--- src/Participant.java
+++ src/Participant.java
@@ -1,17 +1,9 @@
-public class Participant {
-    private String id;
-    private String name;
-    private String eMail;
-    private String preffedGame;
-    private int skillLevel;
-    private String preffedRole;
-
-    public Participant(String id, String name, String eMail, String preffedGame, int skillLevel, String preffedRole) {
-        this.id = id;
-        this.name = name;
-        this.eMail = eMail;
-        this.preffedGame = preffedGame;
-        this.skillLevel = skillLevel;
-        this.preffedRole = preffedRole;
-    }
-}
+public record Participant(
+        String id,
+        String name,
+        String email,
+        String preferredGame,
+        int skillLevel,           // 1–10
+        String preferredRole,
+        int personalityScore     // 20–100
+) {}
