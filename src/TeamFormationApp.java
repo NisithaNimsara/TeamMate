d4c11e8c49893853ce6bc57c5170d428eedec3af
NisithaNimsara <kvsnsd@gmail.com>
2025-11-13 23:20:13 +0530

    Main menu established

--- src/TeamFormationApp.java
+++ src/TeamFormationApp.java
@@ -1,5 +1,23 @@
+import java.util.*;
+
 public class TeamFormationApp {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n==== Main ====");
+            System.out.println("1) Participant");
+            System.out.println("2) Organizer");
+            System.out.println("0) Exit");
+            System.out.print("Choose: ");
+            String ch = sc.nextLine().trim();
+            switch (ch) {
+                case "1" -> System.out.println("Participant");
+                case "2" -> System.out.println("Organizer");
+                case "0" -> { System.out.println("Bye!"); return; }
+                default -> System.out.println("Invalid option.");
+            }
+        }
 
     }
 }
