11cb8230aa97ff1c8ec2bdc2771bf35374cc23cc
NisithaNimsara <kvsnsd@gmail.com>
2025-11-17 23:03:29 +0530

    delete and recreate all -> V2

--- src/Model/ImportResult.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-// this will give the Import Row, and ignore row count
-// Used to summarize how many records were successfully imported or skipped.
-
-public record ImportResult(int importedCount, int ignoredCount) {}
-
