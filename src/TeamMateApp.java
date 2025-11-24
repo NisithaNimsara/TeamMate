a1a5f4739e7826ebf3d54bf33e5fc8b123329dde
NisithaNimsara <kvsnsd@gmail.com>
2025-11-24 17:58:05 +0530

    logger implements to TeamMateApp.java

--- src/TeamMateApp.java
+++ src/TeamMateApp.java
@@ -7,26 +7,42 @@ import Models.TeamBuilder;
 import ValidatorHelp.ConsoleInput;
 
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class TeamMateApp {
 
     private static final String SYSTEM_PARTICIPANT_FILE = "participants_sample.csv";
 
+    private static final Logger logger = Logger.getLogger(TeamMateApp.class.getName());
+
     public static void main(String[] args) {
-        Scanner scanner =  new Scanner(System.in);
-        ConsoleInput input = new ConsoleInput(scanner);
-        PersonalityClassifier classifier = new PersonalityClassifier();
-        ParticipantRepository repository = new ParticipantRepository(SYSTEM_PARTICIPANT_FILE);
-        TeamBuilder teamBuilder = new TeamBuilder();
 
-        //controllers
-        ParticipantController participantController = new ParticipantController(classifier, repository, input);
-        OrganizerController organizerController = new OrganizerController(repository, input, teamBuilder);
+        logger.info("TeamMate application Starting");
+        logger.info("System participant file configured as: " + SYSTEM_PARTICIPANT_FILE);
 
-        AppController appController = new AppController(input, participantController, organizerController);
+        try{
+            Scanner scanner = new Scanner(System.in);
+            ConsoleInput input = new ConsoleInput(scanner);
 
-        //------------------------------------------------------------------------------------------------------------
-        //Main app run
-        appController.run();
+            PersonalityClassifier classifier = new PersonalityClassifier();
+            ParticipantRepository repository = new ParticipantRepository(SYSTEM_PARTICIPANT_FILE);
+            TeamBuilder teamBuilder = new TeamBuilder();
+
+            //controllers
+            ParticipantController participantController = new ParticipantController(classifier, repository, input);
+            OrganizerController organizerController = new OrganizerController(repository, input, teamBuilder);
+            AppController appController = new AppController(input, participantController, organizerController);
+
+            //------------------------------------------------------------------------------------------------------------
+            //Main app run
+            appController.run();
+
+        } catch (Exception e) {
+            logger.log(Level.SEVERE,"Unexpected error in TeamMateApp main method.", e);
+        }
+
+        logger.info("TeamMate application Terminated");
     }
 }
