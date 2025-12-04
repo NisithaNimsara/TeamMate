663a7a61524e7b5600a9be74d02ee209f712770d
NisithaNimsara <kvsnsd@gmail.com>
2025-12-05 01:02:35 +0530

    modifications

--- Tests/Controllers/ParticipantControllerTest.java
+++
@@ -1,6 +0,0 @@
-package Controllers;
-
-import static org.junit.jupiter.api.Assertions.*;
-class ParticipantControllerTest {
-  
-}
--- src/ValidatorHelp/InvalidParticipantException.java
+++
@@ -1,9 +0,0 @@
-package ValidatorHelp;
-
-public class InvalidParticipantException extends Exception {
-
-    //constructor, this create custom exception used to report any invalid participant data.
-    public InvalidParticipantException(String message) {
-        super(message);
-    }
-}
